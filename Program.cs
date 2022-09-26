b513be42ba336bce2d76999ce2e283c30007f2af
Ajgooor <Igorun1@icloud.com>
2022-09-26 16:55:48 +0200

    dodalem nowy plik

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ajgooor_repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
